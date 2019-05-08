b802a3768bc12068c675e5408de6156a0faca148
Ray <ray@optymyze.com>
2019-05-08 17:33:48 +0530

    deploy to server

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-const Index = () => (
-  <div>
-    <h1>Hello Next.js</h1>
-  <p>this is index page</p>
-  </div>
-);
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
 
-export default Index;
+function PostLink(props) {
+  return (
+    <li>
+      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  )
+}
+
+export default function Index() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" title="Hello Next.js" />
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  )
+}
