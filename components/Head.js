0fb63bff63041a6e71a403b9a3414e8bd7856ba6
raydedon <raydedon@gmail.com>
2019-05-22 22:58:00 +0530

    favicon

--- components/Head.js
+++ components/Head.js
@@ -15,6 +15,7 @@ const Head = ({ title, description }) => (
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<meta name="theme-color" content="#000000" />
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
+		<link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
 	</NextHead>
 );
 
